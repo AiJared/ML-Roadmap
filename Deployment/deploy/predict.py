f8da223a339974841df2e279e809d8d7d793aa67
Kerriea-star <ojared511@gmail.com>
2023-06-15 10:26:14 +0300

    started creating a churn prediction service

--- Deployment/deploy/predict.py
+++ Deployment/deploy/predict.py
@@ -2,6 +2,10 @@
 
 import pickle
 
+from flask import Flask
+from flask import request
+from flask import jsonify
+
 
 model_file = "model_C=1.0.bin"
 
@@ -9,32 +13,20 @@ model_file = "model_C=1.0.bin"
 with open(model_file, "rb") as f_in:
     dv, model = pickle.load(f_in)
 
+app = Flask('churn')
 
-customer = {
-    'gender': 'female',
-    'seniorcitizen': 0,
-    'partner': 'yes',
-    'dependents': 'no',
-    'phoneservice': 'no',
-    'multiplelines': 'no_phone_service',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'no',
-    'onlinebackup': 'yes',
-    'deviceprotection': 'no',
-    'techsupport': 'no',
-    'streamingtv': 'no',
-    'streamingmovies': 'no',
-    'contract': 'month-to-month',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'electronic_check',
-    'tenure': 1,
-    'monthlycharges': 29.85,
-    'totalcharges': 29.85
-}
+@app.route("/predict", methods=["POST"])
+def predict(customer):
+    customer = request.get_json()
+    X= dv.transform([customer])
+    y_pred= model.predict_proba(X)[0, 1]
+    churn = y_pred >= 0.5
 
-X= dv.transform([customer])
-y_pred= model.predict_proba(X)[0, 1]
+    result = {
+        "churn_probability": y_pred,
+        "churn": churn
+    }
+    return jsonify(result)
 
-
-print("input", customer)
-print("churn probability", y_pred)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=9696)
